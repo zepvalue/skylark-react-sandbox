2112a4e988190cafbe4f4d485b961b303f9ccecc
Giuseppe Zep Valentino <zepvalue@gmail.com>
2019-10-02 18:43:49 -0400

    (Testing) Unit Tests on the App component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import destinationData from 'data/destination-cancun.json';
 import SearchForm from 'components/SearchForm';
 
 const App = () => {
-  const [responseData, setResponseData] = useState('');
+  const [responseData, setResponseData] = useState(null);
   const { hotels } = destinationData;
   return (
     <div className="App">
       <header className="App-header">
         <div id="App-Container">
-        <div id="header_container">
-          <div id="header_text">Booking is just the beginning</div>
-          <div id="header_description">Skylark combines real time technology with human expertise.</div>
+          <div id="header_container">
+            <div id="header_text">Booking is just the beginning</div>
+            <div id="header_description">Skylark combines real time technology with human expertise.</div>
+          </div>
+          <SearchForm list={hotels} setResponseData={setResponseData} />
+          <div>{responseData}</div>
         </div>
-        <SearchForm list={hotels} setResponseData={setResponseData} />
-        <div id={"response_container"}>{responseData}</div>
-      </div>
       </header>
     </div>
   );
